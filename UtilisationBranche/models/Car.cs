6ab4241cca0f0e69a91a682312aa839634116aa7
forma300 <forma300@FORMA300>
2019-10-31 12:25:04 +0100

    Add model "Car"

---
+++ UtilisationBranche/models/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilisationBranche.models
+{
+    class Car
+    {
+        public string Color { get; set; }
+
+        public int Power { get; set; }
+    }
+}
